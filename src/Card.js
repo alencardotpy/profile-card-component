b8aee2b594fe5cd6712a2a70385ead35e59a7f25
Carlos Alencar <68407281+alencardotpy@users.noreply.github.com>
2022-07-06 09:31:53 -0300

    Update Card.js

--- src/Card.js
+++ src/Card.js
@@ -1,5 +1,6 @@
 import "./Card.css";
 import { Component } from "react";
+import profilePic from "./image-victor.jpg";
 
 class Card extends Component {
   constructor(props) {
@@ -7,6 +8,7 @@ class Card extends Component {
     this.state = {
       username: "Victor Crest",
       userage: "26",
+      userpic: "./image-victor.jpg",
       userlocation: "London",
       followers: "80K",
       likes: "803K",
@@ -17,6 +19,31 @@ class Card extends Component {
     return (
       <div className="card">
         <Header />
+        <UserProfilePicture userProfilePicture={profilePic} />
+        <ProfileContainer>
+          <div className="card__container-title">
+            <UserName userName={this.state.username} />
+            <UserAge userAge={this.state.userage} />
+          </div>
+          <UserLocation userLocation={this.state.userlocation} />
+          <span className="card__divider" />
+          <div className="card__stats-container">
+            <div className="card__stats">
+              <UserFollowers userFollowers={this.state.followers} />
+              <p className="card__stats-label">Followers</p>
+            </div>
+            <div className="card__stats">
+              {" "}
+              <UserLikes userLikes={this.state.likes} />
+              <p className="card__stats-label">Likes</p>
+            </div>
+            <div className="card__stats">
+              {" "}
+              <UserPhotos userPhotos={this.state.photos} />
+              <p className="card__stats-label">Photos</p>
+            </div>
+          </div>
+        </ProfileContainer>
       </div>
     );
   }
@@ -31,7 +58,13 @@ const Header = () => {
 };
 
 const UserProfilePicture = (props) => {
-  return <img src={props.userProfilePicture} alt="User profile picture" />;
+  return (
+    <img
+      className="card__profile-picture"
+      src={props.userProfilePicture}
+      alt="User profile"
+    />
+  );
 };
 
 const UserName = (props) => {
@@ -39,19 +72,23 @@ const UserName = (props) => {
 };
 
 const UserAge = (props) => {
-  return <p className="card__user-location">{props.userAge}</p>;
+  return <p className="card__user-age">{props.userAge}</p>;
 };
 
 const UserLocation = (props) => {
   return <p className="card__user-location">{props.userLocation}</p>;
 };
 
+const UserFollowers = (props) => {
+  return <p className="card__followers card__stats-title">{props.userFollowers}</p>;
+};
+
 const UserLikes = (props) => {
-  return <p className="card__likes">{props.userLikes}</p>;
+  return <p className="card__likes card__stats-title">{props.userLikes}</p>;
 };
 
 const UserPhotos = (props) => {
-  return <p className="card__photos">{props.userPhotos}</p>;
+  return <p className="card__photos card__stats-title">{props.userPhotos}</p>;
 };
 
 export default Card;
